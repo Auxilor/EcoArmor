e02241b740a131b3e80a061e798065c4c2306333
Auxilor <will@willfp.com>
2021-01-20 20:17:11 +0000

    Prevented sets.yml and tiers.yml from updating

--- eco-core/core-plugin/src/main/java/com/willfp/ecoarmor/config/configs/Sets.java
+++ eco-core/core-plugin/src/main/java/com/willfp/ecoarmor/config/configs/Sets.java
@@ -1,13 +1,13 @@
 package com.willfp.ecoarmor.config.configs;
 
-import com.willfp.eco.util.config.BaseConfig;
+import com.willfp.eco.util.config.StaticBaseConfig;
 import com.willfp.ecoarmor.EcoArmorPlugin;
 
-public class Sets extends BaseConfig {
+public class Sets extends StaticBaseConfig {
     /**
      * sets.yml.
      */
     public Sets() {
-        super("sets", false, EcoArmorPlugin.getInstance());
+        super("sets", EcoArmorPlugin.getInstance());
     }
 }
--- eco-core/core-plugin/src/main/java/com/willfp/ecoarmor/config/configs/Tiers.java
+++ eco-core/core-plugin/src/main/java/com/willfp/ecoarmor/config/configs/Tiers.java
@@ -1,13 +1,13 @@
 package com.willfp.ecoarmor.config.configs;
 
-import com.willfp.eco.util.config.BaseConfig;
+import com.willfp.eco.util.config.StaticBaseConfig;
 import com.willfp.ecoarmor.EcoArmorPlugin;
 
-public class Tiers extends BaseConfig {
+public class Tiers extends StaticBaseConfig {
     /**
      * sets.yml.
      */
     public Tiers() {
-        super("tiers", false, EcoArmorPlugin.getInstance());
+        super("tiers", EcoArmorPlugin.getInstance());
     }
 }
