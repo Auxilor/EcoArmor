3596cfcddab9e27b61bf2d7fb71237e5503ec7b0
Auxilor <will@willfp.com>
2021-02-07 20:40:32 +0000

    Fixed Sets.yml

--- eco-core/core-plugin/src/main/java/com/willfp/ecoarmor/config/configs/Sets.java
+++ eco-core/core-plugin/src/main/java/com/willfp/ecoarmor/config/configs/Sets.java
@@ -1,19 +1,13 @@
 package com.willfp.ecoarmor.config.configs;
 
-import com.willfp.eco.util.config.BaseConfig;
+import com.willfp.eco.util.config.StaticBaseConfig;
 import com.willfp.ecoarmor.EcoArmorPlugin;
 
-public class Sets extends BaseConfig {
+public class Sets extends StaticBaseConfig {
     /**
      * sets.yml.
      */
     public Sets() {
-        super("sets",
-                false,
-                EcoArmorPlugin.getInstance(),
-                "set-bonus.",
-                "potion-effects.",
-                ".enchants."
-        );
+        super("sets", EcoArmorPlugin.getInstance());
     }
 }
