df3afc066050371efd7ecb492d40ab209a3b95d4
Auxilor <will@willfp.com>
2021-03-27 18:25:02 +0000

    Updated to eco 4.2.0 display

--- eco-core/core-plugin/src/main/java/com/willfp/ecoarmor/display/ArmorDisplay.java
+++ eco-core/core-plugin/src/main/java/com/willfp/ecoarmor/display/ArmorDisplay.java
@@ -28,7 +28,8 @@ public class ArmorDisplay extends DisplayModule {
     }
 
     @Override
-    protected void display(@NotNull final ItemStack itemStack) {
+    protected void display(@NotNull final ItemStack itemStack,
+                           @NotNull final Object... args) {
         ItemMeta meta = itemStack.getItemMeta();
         if (meta == null) {
             return;
@@ -94,10 +95,5 @@ public class ArmorDisplay extends DisplayModule {
         }
 
         itemStack.setItemMeta(meta);
-    }
-
-    @Override
-    protected void revert(@NotNull final ItemStack itemStack) {
-
     }
 }
