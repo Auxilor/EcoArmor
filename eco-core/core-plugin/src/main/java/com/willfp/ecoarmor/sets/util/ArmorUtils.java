5270be8fd249f0f6bfbcb0a0bb9a3bb66ceecbaf
Auxilor <william.favierparsons1@gmail.com>
2021-06-11 12:37:44 +0100

    Fixed unneeded import

--- eco-core/core-plugin/src/main/java/com/willfp/ecoarmor/sets/util/ArmorUtils.java
+++ eco-core/core-plugin/src/main/java/com/willfp/ecoarmor/sets/util/ArmorUtils.java
@@ -19,7 +19,6 @@ import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.xml.bind.annotation.XmlType;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
