3e0ac767a1c3c1a186ef8352765a8d471f49a915
Auxilor <william.favierparsons1@gmail.com>
2021-06-24 13:32:57 +0100

    Added enchantments to json token

--- eco-core/core-plugin/src/main/java/com/willfp/ecoarmor/commands/CommandEaeditor.java
+++ eco-core/core-plugin/src/main/java/com/willfp/ecoarmor/commands/CommandEaeditor.java
@@ -2,12 +2,17 @@ package com.willfp.ecoarmor.commands;
 
 import com.willfp.eco.core.EcoPlugin;
 import com.willfp.eco.core.command.AbstractCommand;
+import com.willfp.eco.core.config.JSONConfig;
 import com.willfp.eco.core.web.Paste;
 import com.willfp.ecoarmor.EcoArmorPlugin;
+import org.bukkit.NamespacedKey;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CommandEaeditor extends AbstractCommand {
     /**
@@ -22,7 +27,10 @@ public class CommandEaeditor extends AbstractCommand {
     @Override
     public void onExecute(@NotNull final CommandSender sender,
                           @NotNull final List<String> args) {
-        String token = new Paste(((EcoArmorPlugin) this.getPlugin()).getEcoArmorJson().toPlaintext()).getHastebinToken();
+        JSONConfig config = ((EcoArmorPlugin) this.getPlugin()).getEcoArmorJson().clone();
+        List<String> enchantKeys = Arrays.stream(Enchantment.values()).map(Enchantment::getKey).map(NamespacedKey::getKey).collect(Collectors.toList());
+        config.set("meta.enchants", enchantKeys);
+        String token = new Paste(config.toPlaintext()).getHastebinToken();
         String message = this.getPlugin().getLangYml().getMessage("open-editor")
                 .replace("%url%", "https://auxilor.io/editor/ecoarmor?token=" + token);
         sender.sendMessage(message);
