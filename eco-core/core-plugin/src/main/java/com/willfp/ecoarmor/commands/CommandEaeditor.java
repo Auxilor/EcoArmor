994da87e3fcba9a16372ad558a99170a5e89869b
Auxilor <william.favierparsons1@gmail.com>
2021-07-04 13:17:14 +0200

    Added coming soon message to /eaeditor

--- eco-core/core-plugin/src/main/java/com/willfp/ecoarmor/commands/CommandEaeditor.java
+++ eco-core/core-plugin/src/main/java/com/willfp/ecoarmor/commands/CommandEaeditor.java
@@ -2,24 +2,11 @@ package com.willfp.ecoarmor.commands;
 
 import com.willfp.eco.core.EcoPlugin;
 import com.willfp.eco.core.command.AbstractCommand;
-import com.willfp.eco.core.config.JSONConfig;
-import com.willfp.eco.core.web.Paste;
-import com.willfp.ecoarmor.EcoArmorPlugin;
-import com.willfp.ecoarmor.conditions.Condition;
-import com.willfp.ecoarmor.conditions.Conditions;
-import com.willfp.ecoarmor.effects.Effect;
-import com.willfp.ecoarmor.effects.Effects;
-import org.bukkit.NamespacedKey;
+import com.willfp.eco.util.StringUtils;
 import org.bukkit.command.CommandSender;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class CommandEaeditor extends AbstractCommand {
     /**
@@ -34,6 +21,7 @@ public class CommandEaeditor extends AbstractCommand {
     @Override
     public void onExecute(@NotNull final CommandSender sender,
                           @NotNull final List<String> args) {
+        /*
         JSONConfig config = ((EcoArmorPlugin) this.getPlugin()).getEcoArmorJson().clone();
         Map<String, Object> meta = new HashMap<>();
 
@@ -54,5 +42,8 @@ public class CommandEaeditor extends AbstractCommand {
         String message = this.getPlugin().getLangYml().getMessage("open-editor")
                 .replace("%url%", "https://auxilor.io/editor/ecoarmor?token=" + token);
         sender.sendMessage(message);
+         */
+
+        sender.sendMessage(StringUtils.translate("&cThe editor is coming soon!"));
     }
 }
