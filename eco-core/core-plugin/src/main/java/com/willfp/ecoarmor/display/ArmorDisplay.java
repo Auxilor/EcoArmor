2126a199b00bd95dea145fcf361f6b73a3f5d57d
Auxilor <william.favierparsons1@gmail.com>
2021-09-19 11:34:22 +0100

    Updated display module to eco 6.8.0

--- eco-core/core-plugin/src/main/java/com/willfp/ecoarmor/display/ArmorDisplay.java
+++ eco-core/core-plugin/src/main/java/com/willfp/ecoarmor/display/ArmorDisplay.java
@@ -29,7 +29,7 @@ public class ArmorDisplay extends DisplayModule {
     }
 
     @Override
-    protected void display(@NotNull final ItemStack itemStack,
+    public void display(@NotNull final ItemStack itemStack,
                            @NotNull final Object... args) {
         ItemMeta meta = itemStack.getItemMeta();
         if (meta == null) {
