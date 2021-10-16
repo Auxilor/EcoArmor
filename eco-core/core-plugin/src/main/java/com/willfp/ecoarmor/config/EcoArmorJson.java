c4d25944ccf3102937a29a232bf45d9684daf5df
Auxilor <william.favierparsons1@gmail.com>
2021-10-16 19:07:20 +0100

    Added config autoupdating for missing sections

--- eco-core/core-plugin/src/main/java/com/willfp/ecoarmor/config/EcoArmorJson.java
+++ eco-core/core-plugin/src/main/java/com/willfp/ecoarmor/config/EcoArmorJson.java
@@ -1,16 +1,17 @@
 package com.willfp.ecoarmor.config;
 
 import com.willfp.eco.core.EcoPlugin;
+import com.willfp.eco.core.config.json.JSONBaseConfig;
 import com.willfp.eco.core.config.json.JSONStaticBaseConfig;
 import org.jetbrains.annotations.NotNull;
 
-public class EcoArmorJson extends JSONStaticBaseConfig {
+public class EcoArmorJson extends JSONBaseConfig {
     /**
      * Create tiers.json.
      *
      * @param plugin Instance of EcoArmor.
      */
     public EcoArmorJson(@NotNull final EcoPlugin plugin) {
-        super("ecoarmor", plugin);
+        super("ecoarmor", false, plugin);
     }
 }
