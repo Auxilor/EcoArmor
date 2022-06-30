c232fe1331bdca4a3e040ad8ad8ff9d3555af0d4
Auxilor <william.favierparsons1@gmail.com>
2022-06-30 21:46:07 +0100

    Fixed issue #31

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoarmor/commands/CommandGive.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoarmor/commands/CommandGive.kt
@@ -2,6 +2,7 @@ package com.willfp.ecoarmor.commands
 
 import com.willfp.eco.core.EcoPlugin
 import com.willfp.eco.core.command.impl.Subcommand
+import com.willfp.eco.core.display.Display
 import com.willfp.ecoarmor.sets.ArmorSets
 import com.willfp.ecoarmor.sets.ArmorSlot
 import com.willfp.ecoarmor.sets.ArmorSlot.Companion.getSlot
@@ -159,7 +160,7 @@ class CommandGive(plugin: EcoPlugin) : Subcommand(plugin, "give", "ecoarmor.comm
             sender.sendMessage(plugin.langYml.getMessage("invalid-item"))
             return
         }
-        for (item in toGive) {
+        for (item in toGive.map { Display.display(it.clone(), reciever) }) {
             item.amount = amount
             reciever.inventory.addItem(item)
         }
