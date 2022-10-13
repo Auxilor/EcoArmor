825f0d39a989633a21ad36e525f5d5b03d495aac
Auxilor <william.favierparsons1@gmail.com>
2022-10-13 20:13:23 +0100

    libreforge-updater

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoarmor/commands/CommandEcoarmor.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoarmor/commands/CommandEcoarmor.kt
@@ -1,13 +1,26 @@
 package com.willfp.ecoarmor.commands
 
-import com.willfp.eco.core.EcoPlugin
 import com.willfp.eco.core.command.impl.PluginCommand
+import com.willfp.ecoarmor.sets.ArmorSets
+import com.willfp.libreforge.LibReforgePlugin
+import com.willfp.libreforge.lrcdb.CommandExport
+import com.willfp.libreforge.lrcdb.CommandImport
+import com.willfp.libreforge.lrcdb.ExportableConfig
 import org.bukkit.command.CommandSender
 
-class CommandEcoarmor(plugin: EcoPlugin) : PluginCommand(plugin, "ecoarmor", "ecoarmor.command.ecoarmor", false) {
+class CommandEcoarmor(plugin: LibReforgePlugin) : PluginCommand(plugin, "ecoarmor", "ecoarmor.command.ecoarmor", false) {
     init {
         addSubcommand(CommandReload(plugin))
             .addSubcommand(CommandGive(plugin))
+            .addSubcommand(CommandImport("sets", plugin))
+            .addSubcommand(CommandExport(plugin) {
+                ArmorSets.values().map {
+                    ExportableConfig(
+                        it.id,
+                        it.config
+                    )
+                }
+            })
     }
 
     override fun onExecute(sender: CommandSender, args: List<String>) {
