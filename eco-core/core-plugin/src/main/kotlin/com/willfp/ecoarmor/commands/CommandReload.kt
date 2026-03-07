99f44d8bfdb09131a38d2e5b3fe6e4ab5b285dff
NicoNeko <nicuch100@gmail.com>
2026-03-07 20:50:11 +0200

    Revert reload

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoarmor/commands/CommandReload.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoarmor/commands/CommandReload.kt
@@ -1,6 +1,5 @@
 package com.willfp.ecoarmor.commands
 
-import com.willfp.eco.core.Prerequisite
 import com.willfp.eco.core.command.impl.Subcommand
 import com.willfp.eco.util.StringUtils
 import com.willfp.eco.util.toNiceString
@@ -14,15 +13,9 @@ object CommandReload : Subcommand(
     false
 ) {
     override fun onExecute(sender: CommandSender, args: List<String>) {
-        val runnable: Runnable = {
-            sender.sendMessage(
-                plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
-                    .replace("%time%", plugin.reloadWithTime().toNiceString())
-            )
-        }
-        if (Prerequisite.HAS_FOLIA.isMet)
-            plugin.scheduler.runTask(runnable) // run on global thread
-        else
-            runnable.run()
+        sender.sendMessage(
+            plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
+                .replace("%time%", plugin.reloadWithTime().toNiceString())
+        )
     }
 }
