a2c3d6ba7e1129ed08994b581adb040dfd3607bc
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-06-15 09:32:06 +0100

    chore: bump paper dependency version (#73)

--- eco-core/core-plugin/build.gradle.kts
+++ eco-core/core-plugin/build.gradle.kts
@@ -2,7 +2,7 @@ group = "com.willfp"
 version = rootProject.version
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
 }
 
 tasks {
